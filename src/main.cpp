6a6766cfcf04245f0446b8d3371e60f04288a26c
Olgerd Ševiakov <olgerd@zenitech.co.uk>
2018-07-16 18:45:48 +0300

    seed or reeed library what da hell???

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,46 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include "mcp_can.h"
 
-void setup() {
-    // put your setup code here, to run once:
+
+unsigned char Flag_Recv = 0;
+unsigned char len = 0;
+unsigned char buf[8];
+char str[20];
+
+
+MCP_CAN CAN(10);                                            // Set CS to pin 10
+
+void setup()
+{
+    Serial.begin(115200);
+
+START_INIT:
+
+    if(CAN_OK == CAN.begin(CAN_500KBPS))                   // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init ok!");
+    }
+    else
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println("Init CAN BUS Shield again");
+        delay(100);
+        goto START_INIT;
+    }
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+
+void loop()
+{
+    if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
+    {
+        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+
+        for(int i = 0; i<len; i++)    // print the data
+        {
+            Serial.print(buf[i]);Serial.print("\t");
+        }
+        Serial.println();
+    }
 }
